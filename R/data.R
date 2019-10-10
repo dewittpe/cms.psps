e99541713b56e3db45bd60701a194377b2e7a85d
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2019-10-10 10:19:41 -0600

    add function to check md5sums of the downloaded data.

--- R/data.R
+++ R/data.R
@@ -28,6 +28,37 @@ psps_download <- function(path = NULL, ...) {
       u = psps_urls())
 }
 
+#' Verify PSPS .zip files via MD5SUM
+#'
+#' @param path directory to the dowloaded PSPS files.
+#' @param ... not currently used.
+#'
+#' @export
+psps_md5sum <- function(path = NULL, ...) {
+
+  # MD5SUMS as of 10 October 2019
+  expected <- list("psps2010.zip" = "6ddd464247da5e419b43a14b5bfeda75",
+                   "psps2011.zip" = "bbfeaa9b9e1580c2f307efbbdee83a98",
+                   "psps2012.zip" = "dbb8546845ac57a11b02fd6f47b01696",
+                   "psps2013.zip" = "dfc05e337caa5e658a03b0de5c91303b",
+                   "psps2014.zip" = "08e0a8c22dbefebac182b2f920e2b274",
+                   "psps2015.zip" = "b774c332e62440bc6173fb68784a3ef8",
+                   "psps2016.zip" = "a34aa471e33fa0feb7426a8dbbfbfb97",
+                   "psps2017.zip" = "7c543ffba37543c55e51f378b8c3a8ec",
+                   "psps2018.zip" = "725f198cb3a080c5e91cb411c1547b24")
+
+  observed <- tools::md5sum(files = paste(path, names(expected), sep = "/"))
+  names(observed) <- basename(names(observed))
+
+  check <-
+    merge(data.frame(file = names(observed), md5sum = observed,             row.names = NULL, stringsAsFactors = FALSE),
+          data.frame(file = names(expected), md5sum = do.call(c, expected), row.names = NULL, stringsAsFactors = FALSE),
+          by = "file",
+          suffixes = c(".observed", ".expected"))
+  check$check <- sapply(check$md5sum.observed == check$md5sum.expected, isTRUE)
+  check
+}
+
 psps_urls <- function() {
   # urls aquired from https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Physician-Supplier-Procedure-Summary/index.html
   # last verification of urls: 10 October 2019
