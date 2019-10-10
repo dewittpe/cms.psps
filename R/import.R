e9be4efbaa3370da52dff34a6bc6933ccb35bc30
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2019-10-10 10:54:12 -0600

    start building import functions

---
+++ R/import.R
@@ -0,0 +1,50 @@
+#' Import Functions for the PSPS data
+#'
+#' @param path path to the directory where the downloaded data is stored.
+#' @param ... additional arguments passed to \code{data.table::fread}
+#'
+#' @return A \code{\link[data.table]{data.table}}
+#'
+#' @name import
+NULL
+
+#' @rdname import
+#' @export
+psps_import_2010 <- function(path = NULL, ...) {
+  if (is.null(path)) {
+    path <- rappdirs::user_data_dir(appname = "cms.psps")
+  }
+
+  import <-
+    lapply(list.files(paste0(path, "/psps2010"), full.names = TRUE, pattern = "082611$"),
+           data.table::fread,
+           sep = "\n",
+           header = FALSE,
+           showProgress = FALSE)
+
+  data.table::set(psps_import_parse(data.table::rbindlist(import)), j = "YEAR", value = 2010L)
+}
+
+psps_import_parse <- function(x) {
+  data.table::set(x, j = "HCPCS_CD",                    value =            substring(x[["V1"]], first =   1L, last =   5L))
+  data.table::set(x, j = "HCPCS_INITIAL_MODIFIER_CD",   value =            substring(x[["V1"]], first =   6L, last =   7L))
+  data.table::set(x, j = "PROVIDER_SPEC_CD",            value =            substring(x[["V1"]], first =   8L, last =   9L))
+  data.table::set(x, j = "CARRIER_NUM",                 value =            substring(x[["V1"]], first =  10L, last =  14L))
+  data.table::set(x, j = "PRICING_LOCALITY_CD",         value =            substring(x[["V1"]], first =  15L, last =  16L))
+  data.table::set(x, j = "TYPE_OF_SERVICE_CD",          value =            substring(x[["V1"]], first =  17L, last =  17L))
+  data.table::set(x, j = "PLACE_OF_SERVICE_CD",         value =            substring(x[["V1"]], first =  18L, last =  19L))
+  data.table::set(x, j = "HCPCS_SECOND_MODIFIER_CD",    value =            substring(x[["V1"]], first =  20L, last =  21L))
+  data.table::set(x, j = "PSPS_SUBMITTED_SERVICE_CNT",  value = as.numeric(substring(x[["V1"]], first =  22L, last =  35L)))
+  data.table::set(x, j = "PSPS_SUBMITTED_CHARGE_AMT",   value = as.numeric(substring(x[["V1"]], first =  36L, last =  48L)))
+  data.table::set(x, j = "PSPS_ALLOWED_CHARGE_AMT",     value = as.numeric(substring(x[["V1"]], first =  49L, last =  61L)))
+  data.table::set(x, j = "PSPS_DENIED_SERVICES_CNT",    value = as.numeric(substring(x[["V1"]], first =  62L, last =  75L)))
+  data.table::set(x, j = "PSPS_DENIED_CHARGE_AMT",      value = as.numeric(substring(x[["V1"]], first =  76L, last =  88L)))
+  data.table::set(x, j = "PSPS_ASSIGNED_SERVICES_CNT",  value = as.numeric(substring(x[["V1"]], first =  89L, last = 102L)))
+  data.table::set(x, j = "PSPS_NCH_PAYMENT_AMT",        value = as.numeric(substring(x[["V1"]], first = 103L, last = 115L)))
+  data.table::set(x, j = "PSPS_HCPCS_ASC_IND_CD",       value =            substring(x[["V1"]], first = 116L, last = 116L))
+  data.table::set(x, j = "PSPS_ERROR_IND_CD",           value =            substring(x[["V1"]], first = 117L, last = 118L))
+  data.table::set(x, j = "HCPCS_BETOS_CD",              value =            substring(x[["V1"]], first = 119L, last = 121L))
+  data.table::set(x, j = "V1",                          value = NULL)
+  x
+}
+
