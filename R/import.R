ba5de037c040eb0b677f046b23c5fa3c13188642
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2019-10-10 11:00:13 -0600

    add import for 2015

--- R/import.R
+++ R/import.R
@@ -93,6 +93,23 @@ psps_import_2014 <- function(path = NULL, ...) {
   data.table::set(psps_import_parse(data.table::rbindlist(import)), j = "YEAR", value = 2014L)
 }
 
+#' @rdname import
+#' @export
+psps_import_2015 <- function(path = NULL, ...) {
+  if (is.null(path)) {
+    path <- rappdirs::user_data_dir(appname = "cms.psps")
+  }
+
+  import <-
+    lapply(list.files(paste0(path, "/psps2015"), full.names = TRUE, pattern = "PSPS\\d{2}\\.txt$"),
+           data.table::fread,
+           sep = "\n",
+           header = FALSE,
+           showProgress = FALSE)
+
+  data.table::set(psps_import_parse(data.table::rbindlist(import)), j = "YEAR", value = 2015L)
+}
+
 
 
 
