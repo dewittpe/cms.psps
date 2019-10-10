10fbfd591e1ab4fa4b7a8bbb9b55415eef858e06
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2019-10-10 10:35:57 -0600

    adds a function for unzipping the downloaded data

--- R/data.R
+++ R/data.R
@@ -30,11 +30,15 @@ psps_download <- function(path = NULL, ...) {
 
 #' Verify PSPS .zip files via MD5SUM
 #'
-#' @param path directory to the dowloaded PSPS files.
+#' @param path directory to the downloaded PSPS files.
 #' @param ... not currently used.
 #'
 #' @export
 psps_md5sum <- function(path = NULL, ...) {
+
+  if (is.null(path)) {
+    path <- rappdirs::user_data_dir(appname = "cms.psps")
+  }
 
   # MD5SUMS as of 10 October 2019
   expected <- list("psps2010.zip" = "6ddd464247da5e419b43a14b5bfeda75",
@@ -57,6 +61,40 @@ psps_md5sum <- function(path = NULL, ...) {
           suffixes = c(".observed", ".expected"))
   check$check <- sapply(check$md5sum.observed == check$md5sum.expected, isTRUE)
   check
+}
+
+#' Unzip Downloaded Data Sets
+#'
+#' @param path directory to the downloaded PSPS files.
+#' @param ... not currently used.
+#'
+#' @export
+psps_unzip <- function(path = NULL) {
+
+  if (is.null(path)) {
+    path <- rappdirs::user_data_dir(appname = "cms.psps")
+  }
+
+  message("Unzipping 2010...")
+  utils::unzip(paste0(path, "/psps2010.zip"), exdir = paste0(path, "/psps2010"))
+  message("Unzipping 2011...")
+  utils::unzip(paste0(path, "/psps2011.zip"), exdir = paste0(path, "/psps2011"))
+  message("Unzipping 2012...")
+  utils::unzip(paste0(path, "/psps2012.zip"), exdir = paste0(path, "/psps2012"))
+  message("Unzipping 2013...")
+  utils::unzip(paste0(path, "/psps2013.zip"), exdir = paste0(path, "/psps2013"))
+  message("Unzipping 2014...")
+  utils::unzip(paste0(path, "/psps2014.zip"), exdir = paste0(path, "/psps2014"))
+  message("Unzipping 2015...")
+  utils::unzip(paste0(path, "/psps2015.zip"), exdir = paste0(path, "/psps2015"))
+  message("Unzipping 2016...")
+  utils::unzip(paste0(path, "/psps2016.zip"), exdir = paste0(path, "/psps2016"))
+  message("Unzipping 2017...")
+  utils::unzip(paste0(path, "/psps2017.zip"), exdir = paste0(path, "/psps2017"))
+  message("Unzipping 2018...")
+  utils::unzip(paste0(path, "/psps2018.zip"), exdir = paste0(path, "/psps2018"))
+
+  invisible(TRUE)
 }
 
 psps_urls <- function() {
